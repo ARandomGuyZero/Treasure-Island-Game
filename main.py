f5fb2efdfd2c142237eb98db1eb9f525b3f3dd8a
Alan Ramos López <118926925+ARandomGuyZero@users.noreply.github.com>
2024-08-28 17:48:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Treasure Island Game
 
 Author: Alan
-Date: August 21th 2024
+Date: August 21st 2024
 
 This is a simple text-based adventure game where the player makes choices to find a hidden treasure on an island.
 The player will be prompted to choose directions and actions, and the outcome will be determined based on their choices.
